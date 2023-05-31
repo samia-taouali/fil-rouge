537f95d5d3abe830c45a67f886d8b4734a26229e
samia-taouali <114834692+samia-taouali@users.noreply.github.com>
2023-05-31 16:59:10 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="connexion.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <div class="logo"><img src="imgs/logo.svg" alt="" srcset=""></div>
+        <div class="section">
+            <form action="" method="post">
+                <div class="first_inputs">
+                    <div class="input1">
+                        <label for="">Adresse email</label>
+                        <input type="text" placeholder="email">
+                    </div>
+                    <div class="input1">
+                        <label for="">Mot de passe</label>
+                        <input type="text" placeholder="mot de passe">
+                    </div>
+                    <div class="btn"> <input type="submit" value="Se connecter"></div>
+                    <p>vous n'avez pas un compte? <span><a href="#" id="word">inscrivez-vous</a></span></p>
+                </div>
+            </form>
+        </div>
+</body>
+
+</html>
---
+++ inscription.php
@@ -0,0 +1,56 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inscription.css">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div class="main">
+        <div class="logo"><img src="imgs/logo.svg" alt="" srcset=""></div>
+        <div class="section">
+            <form action="" method="post">
+                <div class="first_inputs">
+                    <div class="input1">
+                        <label for="">Nom</label>
+                        <input type="text" placeholder="Nom">
+                    </div>
+                    <div class="input1">
+                        <label for="">Prénom</label>
+                        <input type="text" placeholder="prénom">
+                    </div>
+                </div>
+                <div class="input2">
+                    <label for="">Email</label>
+                    <input type="text" placeholder="email">
+                </div>
+                <div class="input2">
+                    <label for="">N°téléphone</label>
+                    <input type="text" placeholder="N°téléphone">
+                </div>
+                <div class="first_inputs">
+
+                    <div class="input1">
+                        <label for="">Mot de passe</label>
+                        <input type="text" placeholder="mot de passse">
+                    </div>
+
+                    <div class="input1">
+                        <label for="">Confirmer mot de passe</label>
+                        <input type="text" placeholder="confirmer">
+                    </div>
+                </div>
+                <div class="btn"> <input type="submit" value="S'inscrire"></div>
+                <p>vous avez déjà un compte? <span><a href="#" id="word">connectez -vous</a></span></p>
+        </div>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
